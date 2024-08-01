720dbb0b5984d6ab1950fbf540f5766c484fd1b1
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-07-31 22:38:38 -0400

    send methods to object

--- metaprogramming.rb
+++ metaprogramming.rb
@@ -1,5 +1,11 @@
 class Account
     attr_accessor :name, :address, :email, :notes
+
+    def assign_values values
+        values.each_key do |k|
+            self.send("#{k}=", values[k])
+        end
+    end
 end
 
 user_info = {
@@ -10,10 +16,6 @@ user_info = {
 }
 
 account = Account.new
-
-account.name = user_info[:name]
-account.address = user_info[:address]
-account.email = user_info[:email]
-account.notes = user_info[:notes]
+account.assign_values(user_info)
 
 puts account.inspect
