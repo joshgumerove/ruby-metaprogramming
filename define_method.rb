1846ed21e92a09c2a85ce41e5e2f3a4ea22aae3f
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-08-01 10:48:28 -0400

    add block logic

--- define_method.rb
+++ define_method.rb
@@ -1,15 +1,41 @@
 class String
-  def self.add_new_method name
-    send(:define_method, name) {
-      puts "You created a method named #{name} and you are in it right now"
+  # def self.add_new_method name
+  #   send(:define_method, name) {
+  #     puts "You created a method named #{name} and you are in it right now"
+  #   }
+  # end
+
+  def self.add_new_method(name, &logic)
+    send(:define_method, name) {|*args|
+      puts "You created a method name #{name}, and you are in it right now"
+      logic.call(*args)
     }
   end
 end
 
-String.add_new_method("Josh")
-String.send(:define_method, "full_name"){
-  puts "Josh Gumerove"
-}
+# String.add_new_method("Josh")
+# String.send(:define_method, "full_name"){
+#   puts "Josh Gumerove"
+# }
 
-"Gumerove".Josh
-"Gumerove".full_name
+# "Gumerove".Josh
+# "Gumerove".full_name
+
+my_string = String.new
+
+String.add_new_method("add_numbers") do |x, y|
+  puts "here is the logic"
+  puts x + y
+end
+
+my_string.add_numbers(20, 40)
+
+def args_test(*args)
+  context = {}
+  args.each do |arg|
+    context[arg] = "test"
+  end
+  puts context
+end
+
+args_test("First", "Second")
